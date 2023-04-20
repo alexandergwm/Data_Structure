2395649591d3c4f9e017c423c48edaf296f27f0b
Gavin <wenmiaogao@163.com>
2023-04-20 17:04:49 +0200

    Sequential search in SSTable

---
+++ Qingdao_University_data_structure/Sequential search.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+#define MAXSIZE 100
+typedef int KeyType;
+typedef int InfoType;
+struct ElemType
+{
+    KeyType key;
+    InfoType otherinfo;
+};
+
+struct SSTable
+{
+    ElemType *R;  // 顺序表地址指针
+    int length; // 顺序表的长度
+};
+
+int SqSearch(SSTable &S, const KeyType e)
+{
+    // 添加关键字
+    S.R[0].key = e;
+    // 顺序比较查找
+    int i;
+    for (i = S.length; S.R[i].key != e;--i)
+    {
+        ;
+    }
+    return i;
+    // 时间复杂度O(n),空间复杂度O(1),ASL = (n+1)/2
+}
+
+
+// 创建顺序表
+void CreateSSTable(SSTable &S)
+{
+    cout << "Please input the length of the table: " << endl;
+    cin >> S.length;
+    S.R = new ElemType[S.length + 1];
+    cout << "Please input the key of the table: " << endl;
+    for (int i = 1; i <= S.length;++i)
+    {
+        cin >> S.R[i].key;
+    }
+}
+
+int main()
+{
+    SSTable S;
+    CreateSSTable(S);
+    int i = SqSearch(S, 3);
+    cout << i << endl;
+    return 0;
+}
